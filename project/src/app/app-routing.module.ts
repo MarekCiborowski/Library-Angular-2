50d558948332b01d46ffa157e3b54b5d8887a13a
Marek Ciborowski <senthi236@gmail.com>
2019-01-15 16:44:37 +0100

    niedziałający (jeszcze) routing

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {OrderComponent} from "./order/order.component";
+import {CartComponent} from "./cart/cart.component";
+import {YourOrdersComponent} from "./your-orders/your-orders.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'order', component: OrderComponent,pathMatch: 'full' },
+  { path: 'cart', component: CartComponent,pathMatch: 'full' },
+  { path: 'yourOrders', component: YourOrdersComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+import { YourOrdersComponent } from './your-orders/your-orders.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    OrderComponent,
+    YourOrdersComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
