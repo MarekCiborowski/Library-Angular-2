ad55bb6261cc7875b967e7a10bf073fb4c4e161b
Marek Ciborowski <senthi236@gmail.com>
2019-01-15 18:01:22 +0100

    RUTING 🦔🦔🦔

--- project/src/app/navigation/navigation.component.ts
+++ project/src/app/navigation/navigation.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  redirect(pagename: string) {
+    console.log(pagename);
+    this.router.navigateByUrl('/'+pagename);
+  }
+
 
 }
